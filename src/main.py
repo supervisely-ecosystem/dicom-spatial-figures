13472c329abca4266c18119dd71f2e6f9d47b1f1
GoldenAnpu <seryios.sych@gmail.com>
2023-09-26 18:16:57 +0200

    upd

--- src/main.py
+++ src/main.py
@@ -19,10 +19,6 @@ if workspace is None:
     sly.logger.warning("You should put correct WORKSPACE_ID value to local.env")
     raise ValueError(f"Workspace with id={workspace_id} not found")
 
-################################    Part 1    ######################################
-###################    Create empty project and dataset    #########################
-############################    Upload volume    ###################################
-
 # create empty project and dataset on server
 project_info = api.project.create(
     workspace.id,
@@ -43,14 +39,6 @@ volume_info = api.volume.upload_nrrd_serie_path(
     path="data/CTChest_nrrd/CTChest.nrrd",
 )
 
-
-################################    Part 2    #####################################
-######################## Create annotations for volume ############################
-###################################################################################
-
-mask3d_path = "data/mask/lung.nrrd"
-image_path = "data/mask/body.png"
-
 # create annotation classes
 lung_class = sly.ObjClass("lung", sly.Mask3D, color=[111, 107, 151])
 body_class = sly.ObjClass("body", sly.Mask3D, color=[209, 192, 129])
@@ -59,22 +47,32 @@ fbody_class = sly.ObjClass("foreign_body", sly.Mask3D, color=[255, 153, 204])
 # update project meta with new classes
 api.project.append_classes(project_info.id, [lung_class, fbody_class, body_class])
 
+################################    NRRD file    ######################################
+
+mask3d_path = "data/mask/lung.nrrd"
+
 # create Mask3D object for 'lung' annotation using NRRD file with 3D Object
 lung_mask = sly.Mask3D.from_file(mask3d_path)
 lung = sly.VolumeObject(lung_class, mask_3d=lung_mask)
 
+###############################    NumPy array    #####################################
 
 # create Mask3D object for 'foreign_body' annotation using NumPy array representing the sphere
 width, height, depth = (512, 512, 139)  # volume shape
 center = np.array([128, 242, 69])  # sphere center in the volume
 radius = 25
 x, y, z = np.ogrid[:width, :height, :depth]
-fbody_mask = (
-    (x - center[0]) ** 2 + (y - center[1]) ** 2 + (z - center[2]) ** 2 <= radius**2
-).astype(np.uint8)
+# Calculate the squared distances from each point to the center
+squared_distances = (x - center[0]) ** 2 + (y - center[1]) ** 2 + (z - center[2]) ** 2
+# Create a boolean mask by checking if squared distances are less than or equal to the square of the radius
+fbody_mask = squared_distances <= radius**2
+fbody_mask = fbody_mask.astype(np.uint8)
 fbody_mask = sly.Mask3D(fbody_mask)
 foreign_body = sly.VolumeObject(fbody_class, mask_3d=fbody_mask)
 
+##################################    Image    ########################################
+
+image_path = "data/mask/body.png"
 
 # create Mask3D object for 'body' annotation using image file
 mask = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
@@ -83,10 +81,6 @@ body_mask = sly.Mask3D(np.zeros(volume_info.file_meta["sizes"], np.bool_))
 # fill this mask with the an image mask for the desired plane
 body_mask.add_mask_2d(mask, plane_name="axial", slice_index=69, origin=[36, 91])
 body = sly.VolumeObject(body_class, mask_3d=body_mask)
-
-################################    Part 3    ######################################
-################### Upload annotation into the volume on server ####################
-####################################################################################
 
 # create volume annotation object
 volume_ann = sly.VolumeAnnotation(
